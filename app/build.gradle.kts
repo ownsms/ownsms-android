c28ca19ef4fa0f929cbc0a0b59c03ba638b043fb
ownsms <297863690+ownsms@users.noreply.github.com>
2026-07-27 10:09:42 +0500

    chore(android): v1.2.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "uz.ownsms.sender"
         minSdk = 26
         targetSdk = 34
-        versionCode = 6
-        versionName = "1.2.2"
+        versionCode = 7
+        versionName = "1.2.3"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
